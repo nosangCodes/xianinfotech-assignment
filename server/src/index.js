a87c80a509155582d47b3ccfe17e684733d4dda6
Nosang Subba <nosangdev@gmail.com>
2025-01-23 14:40:10 +0530

    created user schema

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { config } from "dotenv";
+// import routes from "./routes";
+import bodyParser from "body-parser";
+import cors from "cors";
+config();
+
+const app = express();
+app.use(bodyParser.json());
+const allowedDomains = ["http://localhost:3000", "https://example.com"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedDomains.indexOf(origin) !== -1) {
+      // If the origin is in the allowed list or no origin (same-origin requests), allow the request
+      callback(null, true);
+    } else {
+      // Otherwise, block the request
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Use CORS middleware with the configured options
+app.use(cors(corsOptions));
+
+// app.use("/api", routes);
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("server listening on ", process.env.PORT || 4000);
+});
---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,7 @@
+export function registerUser(userData){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
